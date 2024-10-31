046d1db9b9601a131723e3bda303075ac60c821b
Guilherme <guichasemartinsgmr@hotmail.com>
2024-10-31 13:48:40 -0300

    first commit

---
+++ resource-operator-java-aws/src/main/java/com/github/gmv/resource_operator_java_aws/ResourceOperatorJavaAwsApplication.java
@@ -0,0 +1,13 @@
+package com.github.gmv.resource_operator_java_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceOperatorJavaAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceOperatorJavaAwsApplication.class, args);
+	}
+
+}
---
+++ resource-operator-java-aws/src/test/java/com/github/gmv/resource_operator_java_aws/ResourceOperatorJavaAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gmv.resource_operator_java_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceOperatorJavaAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
